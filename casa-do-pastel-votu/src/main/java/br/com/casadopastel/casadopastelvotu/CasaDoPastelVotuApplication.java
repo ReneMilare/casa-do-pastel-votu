101770727404bf9a092c31f107070aa151c08191
ReneMilare <milarerp@gmail.com>
2021-09-12 10:40:39 -0300

    gerando projeto MVC

---
+++ casa-do-pastel-votu/src/main/java/br/com/casadopastel/casadopastelvotu/CasaDoPastelVotuApplication.java
@@ -0,0 +1,13 @@
+package br.com.casadopastel.casadopastelvotu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasaDoPastelVotuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasaDoPastelVotuApplication.class, args);
+	}
+
+}
---
+++ casa-do-pastel-votu/src/test/java/br/com/casadopastel/casadopastelvotu/CasaDoPastelVotuApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.casadopastel.casadopastelvotu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasaDoPastelVotuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
