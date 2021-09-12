dc95cce5168c6797eb87805e4f90ae0624a55d15
ReneMilare <milarerp@gmail.com>
2021-09-12 15:35:08 -0300

    criado tabela produtos no banco de dados

---
+++ casa-do-pastel-votu/src/main/java/br/com/casadopastel/casadopastelvotu/model/Produto.java
@@ -0,0 +1,31 @@
+package br.com.casadopastel.casadopastelvotu.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+import java.math.BigDecimal;
+
+import lombok.Data;
+
+
+@Entity
+@Table(name = "produtos")
+@Data
+public class Produto {
+
+  @Id 
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @NotBlank
+  private String nomeProduto;
+  @NotBlank
+  private String descricaoProduto;
+  @NotBlank
+  private BigDecimal preco;
+  
+}
